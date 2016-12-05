e0ada97b05c8268ff53635fdf2d2686c065728ed
Rodsevich <neton_12@hotmail.com>
2016-12-05 07:29:23 -0300

    Script de instalacion

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build &> /dev/null
 cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DQML_INSTALL_DIR=lib/qt/qml ..
 sudo make install
 plasmapkg2 -i ../org.kde.tomatodoing || plasmapkg2 -u ../org.kde.tomatodoing
 kbuildsycoca5
